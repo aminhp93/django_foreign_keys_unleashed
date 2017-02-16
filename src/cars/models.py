b70bb047e39182870aed4b3c11d2fd3dcc6735d6
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-16 09:37:49 -0800

    2 - First Foreign Key Relation

--- src/cars/models.py
+++ src/cars/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class Car(models.Model):
+	user = models.ForeignKey(User)
+	name = models.CharField(max_length=120)
+
+	def __str__(self):
+		return self.name
