75135b89dd137e086b2c558643ce4ceac46f4923
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-16 18:43:46 -0800

    10 - Save Request User to Model in Admin

--- src/cars/admin.py
+++ src/cars/admin.py
@@ -6,7 +6,15 @@ from .models import Car
 class CarAdmin(admin.ModelAdmin):
 	search_fields = ["user__username", "user__email", "name", "user__id"]
 	raw_id_fields = ["user"]
+	readonly_fields = ["user"]
+	
 	class Meta:
 		model = Car
+
+	def save_model(self, request, obj, form, change):
+		if change:
+			obj.updated_by = request.user
+		obj.save()
+
 
 admin.site.register(Car, CarAdmin)
