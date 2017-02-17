68aaf2f8aa018631d6ff6c28cb276bab7a5afa6c
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-16 18:49:38 -0800

    11 - Model Admin Methods for User Foreign Key

--- src/cars/admin.py
+++ src/cars/admin.py
@@ -7,7 +7,7 @@ class CarAdmin(admin.ModelAdmin):
 	search_fields = ["user__username", "user__email", "name", "user__id"]
 	raw_id_fields = ["user"]
 	readonly_fields = ["user"]
-	
+
 	class Meta:
 		model = Car
 
@@ -15,6 +15,25 @@ class CarAdmin(admin.ModelAdmin):
 		if change:
 			obj.updated_by = request.user
 		obj.save()
+
+	# def get_queryset(self, request):
+    #     qs = super(CarAdmin, self).get_queryset(request)
+    #     if request.user.is_superuser: #request.user.is_staff
+    #         return qs
+    #     return qs.filter(user=request.user)
+
+    # def has_change_permission(self, request, obj=None):
+    #     """
+    #     means is staff (request.user.is_staff)
+    #     """
+    #     if not obj:
+    #         return True 
+    #     return obj.user == request.user or request.user.is_superuser
+
+    # def get_readonly_fields(self, request, *args, **kwargs):
+    #     if request.user.is_superuser:
+    #         return []
+    #     return ['user']
 
 
 admin.site.register(Car, CarAdmin)
