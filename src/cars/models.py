2d7d21bc3dc7e53e223f5939c3a6b604455e95e2
aminhp93 <minhpn.org.ec@gmail.com>
2017-02-16 18:22:46 -0800

    7 - On Delete

--- src/cars/models.py
+++ src/cars/models.py
@@ -1,11 +1,22 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
 User = settings.AUTH_USER_MODEL
 
+
+def set_delete_user():
+	User = get_user_model()
+	return User.objects.get_or_create(username='deleted')[0]
+
+
+
 class Car(models.Model):
-	first_owner = models.OneToOneField(User)
+	# user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+	# user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=1)
+	user = models.ForeignKey(User, on_delete=models.SET(set_delete_user))
+	# first_owner = models.OneToOneField(User)
 	# user = models.ForeignKey(User)
 	# driver = models.ManyToManyField(User)
 	name = models.CharField(max_length=120)
